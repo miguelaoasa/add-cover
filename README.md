eff71e6301bf1ef1ad14e09da88e97473a955d64
miguelaoasa <184927843+miguelaoasa@users.noreply.github.com>
2025-08-14 17:12:55 +0200

    add add-cover files

---
+++ README.md
@@ -0,0 +1,16 @@
+# add-cover
+This Python script adds a cover image to one or more MP3 audio files. It uses the mutagen library to manipulate ID3 tags, allowing users to embed JPEG or PNG images as album art. 
+
+## Usage
+```
+usage: add_cover.py [-h] cover files [files ...]
+
+Add a cover image to MP3 files
+
+positional arguments:
+  cover       Path to the cover image
+  files       List of MP3 files to which the cover will be added
+
+options:
+  -h, --help  show this help message and exit
+```
---
+++ add_cover.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from mimetypes import guess_type
+from mutagen.id3 import APIC, ID3
+
+import argparse
+import os
+import sys
+
+def add_cover(cover, file):
+    mime = guess_type(cover)[0]
+    if mime not in ["image/jpeg", "image/png"]:
+        print(f"[!!] Invalid cover MIME type.")
+        sys.exit(1)
+
+    audio = ID3(file)
+
+    audio.add(
+            APIC(mime=mime, type=3,
+                 data=open(cover, "rb").read()
+                ))
+
+    audio.save()
+
+
+def check_file(file):
+    if not os.path.isfile(file):
+        print(f"[!] {file} not such file.")
+        sys.exit(1)
+
+    if not os.access(file, os.R_OK):
+        print(f"[!] {file} access denied.")
+        sys.exit(1)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Add a cover image to MP3 files")
+    parser.add_argument("cover", help="Path to the cover image")
+    parser.add_argument("files", nargs='+', help="List of MP3 files to which the cover will be added")
+
+    args = parser.parse_args()
+
+    cover = args.cover
+    check_file(cover)
+
+    for file in args.files:
+        check_file(file)
+        if guess_type(file)[0] != "audio/mpeg":
+            print(f"[!!] {file} invalid MIME type.")
+            continue
+
+        add_cover(cover, file)
+
+
+if __name__ == "__main__":
+    main()
